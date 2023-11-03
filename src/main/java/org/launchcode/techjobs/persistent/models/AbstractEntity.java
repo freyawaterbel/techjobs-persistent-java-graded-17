2452edd6ab1ee0dde829fb3edebc75cef60761ae
freyawaterbel <freyawaterbel@gmail.com>
2023-11-02 20:29:10 -0400

    Tasks complete again

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private int id;
 
     @NotBlank
